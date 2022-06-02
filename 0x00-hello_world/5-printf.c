56919d68fbdb41ada913d889db6ae6e42469ae3f
yberhanu <yohannes.birh@gmail.com>
2022-06-02 11:43:54 -0700

    push 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,129 @@
+#include <stdio.h>
+  
+
+
+/**                                                                                                                                                      
+
+                                                                                                                                                         
+
+ * main - Return 0                                                                                                                                        
+
+ *                                                                                                                                                        
+
+ */
+
+int main(void)
+  
+{
+  
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  
+  return (0);
+  
+}  #include <stdio.h>
+
+
+
+   /**                                                                                                                                                      
+
+                                                                                                                                                         
+
+    * main - Return 0                                                                                                                                        
+
+    *                                                                                                                                                        
+
+    */
+
+int main(void)
+
+{
+  
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  
+  return (0);
+  
+}  #include <stdio.h>
+
+
+
+   /**                                                                                                                                                      
+
+                                                                                                                                                         
+
+    * main - Return 0                                                                                                                                        
+
+    *                                                                                                                                                        
+
+    */
+
+int main(void)
+
+{
+  
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  
+  return (0);
+  
+}  #include <stdio.h>
+
+
+
+   /**                                                                                                                                                      
+
+                                                                                                                                                         
+
+    * main - Return 0                                                                                                                                        
+
+    *                                                                                                                                                        
+
+    */
+
+int main(void)
+
+{
+  
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  
+  return (0);
+  
+}
+
+int main(void)
+  
+{
+  
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  
+  return (0);
+  
+}  #include <stdio.h>
+
+   /**
+
+    * main - Entry point
+
+    *
+
+    * Return: Always 0 (Success)
+
+    */
+
+int main(void)
+
+{
+  
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  
+  return (0);
+  
+}#include <stdio.h>
+
+ /**
+* 
+* Return 0
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
