facfa632850135d9076f474c891de4c7f3b67df6
yberhanu <yohannes.birh@gmail.com>
2022-06-02 11:59:31 -0700

    push 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
-* main 
+* main - Entry point
 *
-* Return 0
-*/
+* Return: Always 0 (Success)
+*/ 
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
