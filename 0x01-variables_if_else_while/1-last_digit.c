3e531d6fab41d8019cb3baf855288bdcd9db32d8
yberhanu <yohannes.birh@gmail.com>
2022-06-03 04:54:10 -0700

    push 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,12 +18,13 @@ if (l > 5)
 {
 printf("Last digit of %d is %d and is greater than 5\n", n, l);
 }
-e1lse if (l == 0)
+else if (l == 0)
 {
 printf("Last digit of %d is %d and is 0\n", n, l);
 }
 else
-{printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
+{
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
 }  
 return (0);
 }
