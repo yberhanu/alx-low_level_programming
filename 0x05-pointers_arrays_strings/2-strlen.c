875bf30ec6a6d5080a458900425bf93bb2c945ad
yberhanu <yohannes.birh@gmail.com>
2022-06-13 11:56:46 -0700

    2-strlen.c pushed

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+include "main.h"
+/**
+* _strlen - returns the length of a string.
+* @s: input string.
+* Return: length of a string.
+*/
+int _strlen(char *s)
+{
+int i  = 0;
+while (*(s + i) != '\0')
+i++;
+return (i);
+}
