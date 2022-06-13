9659c5280fb1283e8250405a9f5eca78dc4eebf9
yberhanu <yohannes.birh@gmail.com>
2022-06-13 12:41:10 -0700

    5-rev_string.c pushed

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,16 +11,11 @@ for (index = len - 1; index >= 0; index--)
 _putchar(s[index]);
 _putchar('\n');
 }
-/**
-*_strlen - returns the length of a string
-* @s: string
-*Return: returns lenght;
-*/
 int _strlen(char *s)
 {
 int count, i;
 i = 0;
-for (count = 0; s[count] != '\0'; count++)
+ for (count = 0; s[count] != '\0'; count++)
 i++;
 return (i);
 }
