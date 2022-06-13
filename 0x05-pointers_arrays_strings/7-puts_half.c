70b343d409ac01918063990543ebe05b65ab5355
yberhanu <yohannes.birh@gmail.com>
2022-06-13 13:08:25 -0700

    7-puts_half.c pushed

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*puts_half - puts half of a string
+*@str: string
+*Return:void
+*/
+void puts_half(char *str)
+{
+int len = _strlen(str);
+if (len % 2 == 0)
+len = len / 2;
+else
+len = (len + 1) / 2;
+while (str[len] != '\0')
+{
+_putchar(str[len]);
+len++;
+}
+_putchar('\n');
+}
