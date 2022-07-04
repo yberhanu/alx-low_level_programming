7ff8b8b85e465278236e2c4389278afc583fb2a5
yberhanu <yohannes.birh@gmail.com>
2022-07-04 11:15:47 -0700

    error solved

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,31 +1,15 @@
 #ifndef DOG_H
-
 #define DOG_H
-
-
-
 #include <stdio.h>
-
 #include <stdlib.h>
-
-
-
 /**
-
- *struct dog - a new type of data
-
- *representing a dog
-
- *@name: name of dog
-
- *@age: age of dog
-
- *@owner: owner's name
-
- */
-
+*struct dog - a new type of data
+*representing a dog
+*@name: name of dog
+*@age: age of dog
+*@owner: owner's name
+*/
 struct dog
-
 {
 char *name;
 float age;
