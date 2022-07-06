1baf315c89250188089def3fdf0a7670d9eeed9c
yberhanu <yohannes.birh@gmail.com>
2022-07-06 10:40:12 -0700

    push 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,13 @@
-include "function_pointers.h"
+#include "function_pointers.h"
 /**
-*print_name - prints a name
-*@name:name to print
-*@f: function which print name
+* print_name - prints a name.
+* @name: input name.
+* @f: function pointer.
+*
+* Return: no return.
 */
 void print_name(char *name, void (*f)(char *))
 {
-if (name == NULL || f == NULL)
-return;
+if (name && f)
 f(name);
 }
