4ed2b2ed822f6bc1185fd996b7fc5872a1d863bf
yberhanu <yohannes.birh@gmail.com>
2022-07-14 11:33:21 -0700

    betty style

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,15 @@
+#include "lists.h"
 #include <stdio.h>
-void __attribute__((constructor)) hare(void);
 /**
-* hare - Prints a string before the
-*   main function is executed.
+* before_main - prints before main
+*
+*
+*
+*
+* Return: void
 */
-void hare(void)
+void  __attribute__((constructor)) before_main()
 {
-printf("You're beat! and yet, you must allow,\n""I bore my house upon my back!\n");
+printf("You\'re beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
 }
