c248ec467cc9bafa6dca01e6017e192c49083bcb
yberhanu <yohannes.birh@gmail.com>
2022-09-01 12:39:20 -0700

    Error resolved

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -7,11 +7,11 @@
 */
 size_t dlistint_len(const dlistint_t *h)
 {
-  int i = 0;
-  while (h != NULL)
-    {
-      i++;
-      h = h->next;
-    }
-  return (i);
+int i = 0;
+while (h != NULL)
+{
+i++;
+h = h->next;
+}
+return (i);
 }
