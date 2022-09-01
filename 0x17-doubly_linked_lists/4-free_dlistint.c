e173cbb11e8a3f11c7551b492d1ad00324e7ee01
yberhanu <yohannes.birh@gmail.com>
2022-09-01 02:16:16 -0700

    function that frees a dlistint_t list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+* free_dlistint - free a doubly linked list
+* @head: the head of list
+* Return: void
+*/
+void free_dlistint(dlistint_t *head)
+{
+  dlistint_t *lst;
+  while (head)
+    {
+      lst = head;
+      head = head->next;
+      free(lst);
+     }
+}
