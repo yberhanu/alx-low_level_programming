a86f45859231dcd3d46dc2d575d7448dee90bd78
yberhanu <yohannes.birh@gmail.com>
2022-09-01 02:30:59 -0700

    add bracket

---
+++ 0x17-doubly_linked_lists/x6-sum_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+* sum_dlistint - sum all the ints in list
+* @head: head of the list
+* Return: sum or 0 if empty
+*/
+int sum_dlistint(dlistint_t *head)
+{
+  int sum = 0;
+  if (!head)
+    return (0);
+  while (head)
+    {
+      sum += head->n;
+      head = head->next;
+    }
+  return (sum);
+}
