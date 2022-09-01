ed0d261ebe6615a9c44da3c75506f78cfdd8ac01
yberhanu <yohannes.birh@gmail.com>
2022-09-01 12:49:04 -0700

    Error resolved

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -6,11 +6,11 @@
 */
 void free_dlistint(dlistint_t *head)
 {
-  dlistint_t *lst;
-  while (head)
-    {
-      lst = head;
-      head = head->next;
-      free(lst);
-     }
+dlistint_t *lst;
+while (head)
+{
+lst = head;
+head = head->next;
+free(lst);
+}
 }
