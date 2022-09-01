217a2618ac4a5ac30cf08dd5489478340d402a26
yberhanu <yohannes.birh@gmail.com>
2022-09-01 12:35:30 -0700

    function that returns the sum of all the data (n)

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -6,13 +6,13 @@
 */
 int sum_dlistint(dlistint_t *head)
 {
-  int sum = 0;
-  if (!head)
-    return (0);
-  while (head)
-    {
-      sum += head->n;
-      head = head->next;
-    }
-  return (sum);
+int sum = 0;
+if (!head)
+return (0);
+while (head)
+{
+sum += head->n;
+head = head->next;
+}
+return (sum);
 }
