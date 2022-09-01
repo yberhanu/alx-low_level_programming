e338132c8f194fa6d3e2c065f4335915b96f7772
yberhanu <yohannes.birh@gmail.com>
2022-09-01 06:21:00 -0700

    remove space

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -7,15 +7,15 @@
 */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-  unsigned int i = 0;
-  if (!head)
-    return (NULL);
-  for (i = 0; i < index; i++)
-    {
-      if (head->next)
-	head = head->next;
-      else
-	return (NULL);
-    }
-  return (head);
+unsigned int i = 0;
+if (!head)
+return (NULL);
+for (i = 0; i < index; i++)
+{
+if (head->next)
+head = head->next;
+else
+return (NULL);
+}
+return (head);
 }
