4729cfc1f20f0dff97ddb0757e1ad8dba91e73ca
yberhanu <104769142+yberhanu@users.noreply.github.com>
2022-09-05 23:44:37 +0300

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -O /run/librand.so https://github.com/yberhanu/alx-low_level_programming/raw/master/0x18-dynamic_libraries/librand.so
+wget -q -O /run/librand.so https://github.com/AbdiAbader/alx-low_level_programming/raw/master/0x18-dynamic_libraries/librand.so
 export LD_PRELOAD=/run/librand.so
