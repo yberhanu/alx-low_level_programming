8ea938e348e943e26987e85e33fae9536ee35e59
yberhanu <yohannes.birh@gmail.com>
2022-09-23 11:30:17 -0700

    Adding Readme

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,12 +1,21 @@
-#include <unistd.h>
+#include "main.h"
 /**
-* _putchar - writes the character c to stdout
-* @c: The character to print
-*
-* Return: On success 1.
-* On error, -1 is returned, and errno is set appropriately.
+*  _strncpy - C function that copies a string, including the
+*  terminating null byte, using at most an inputted number of bytes.
+*  If the length of the source string is less than the maximum byte number,
+*  the remainder of the destination string is filled with null bytes.
+*  Works identically to the standard library function `strncpy`.
+*@dest: buffer storing the string copy
+*@src:the source string
+*@n:max nummber of byte copied
+*Return: returns
 */
-int _putchar(char c)
+char *_strncpy(char *dest, char *src, int n)
 {
-return (write(1, &c, 1));
+int i;
+for (i = 0; i < n && src[i] != '\0'; i++)
+dest[i] = src[i];
+for ( ; i < n; i++)
+dest[i] = '\0';
+return (dest);
 }
